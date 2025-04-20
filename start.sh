a5c292bc54323a747c2ccfa062f85c9015db0449
Drezatam <tedsnkrs@gmail.com>
2025-04-20 11:57:53 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,7 @@
-cd "$(dirname "$0")"
+#!/bin/bash
+
+# Installer les navigateurs Playwright (indispensable pour fonctionner)
+playwright install --with-deps
+
+# Lancer le script Python
 python vinted_bot.py
